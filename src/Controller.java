8fe03fff4a0243c682d4fe85526eec17795dffe6
nial0003 <nial0003@stud.kea.dk>
2024-11-07 15:07:51 +0100

    removed unused methods.

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
@@ -8,27 +7,27 @@ public class Controller {
     private MovieCollection movieCollection = new MovieCollection();
     private FileHandler fh = new FileHandler();
 
-    public void addMovie (String titel, String director, int yearCreated, boolean isInColor, int lengthInMinutes, String genre) {
+    public void addMovie(String titel, String director, int yearCreated, boolean isInColor, int lengthInMinutes, String genre) {
         movieCollection.addMovie(titel, director, yearCreated, isInColor, lengthInMinutes, genre);
     }
 
-    public String showMovies(){
+    public String showMovies() {
         return movieCollection.showMovies(movieCollection.getMovies());
     }
 
-    public String findMovie (String input) {
+    public String findMovie(String input) {
         return movieCollection.findMovie(input);
     }
 
-    public ArrayList<Movie> findMovieObject(String input){
+    public ArrayList<Movie> findMovieObject(String input) {
         return movieCollection.findMovieObject(input);
     }
 
-    public String editProperty(String partToEdit, String newValue, Movie movie){
+    public String editProperty(String partToEdit, String newValue, Movie movie) {
         return movie.editProperty(partToEdit, newValue);
     }
 
-    public String removeMovie(Movie movieToBeRemoved){
+    public String removeMovie(Movie movieToBeRemoved) {
         return movieCollection.removeMovie(movieToBeRemoved);
     }
 
@@ -50,27 +49,25 @@ public class Controller {
         movieCollection.sortMoviesByLengthInMinutes();
     }
 
-    public ArrayList<Movie> loadFromFile(){
+    public ArrayList<Movie> loadFromFile() {
         return fh.loadFromFile();
     }
 
-    public void setListOfMovies(ArrayList<Movie> listOfMovies){
+    public void setListOfMovies(ArrayList<Movie> listOfMovies) {
         movieCollection.setListOfMovies(listOfMovies);
     }
 
-    public void clearMovieList(){
+    public void clearMovieList() {
         movieCollection.clearMovieList();
     }
 
-    public void deleteOldFileAndCreateNewEmptyFile(){
+    public void deleteOldFileAndCreateNewEmptyFile() {
         fh.deleteOldFileAndCreateNewEmptyFile();
     }
 
-    public String firstLetterToUpper(String str){
+    public String firstLetterToUpper(String str) {
         return Movie.firstLetterToUpper(str);
     }
-}
-
 
     public Comparator<Movie> getComparatorByTitle() {
         return new TitleComparator();
